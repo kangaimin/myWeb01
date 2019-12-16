f0b34e6d9284c0404dc1efedd3bafa8ca9057940
kangam <kangam1@163.com>
2019-12-16 08:35:25 +0800

    Initial commit

---
+++ src/com/atguigu/test/Book.java
@@ -0,0 +1,9 @@
+package com.atguigu.test;
+
+/**
+ * @author administrator
+ * @create 2019-12-14 10:57
+ */
+public class Book {
+
+}
---
+++ src/com/atguigu/test/Hello.java
@@ -0,0 +1,13 @@
+package com.atguigu.test;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        List list =new ArrayList();//手工导入alt+enter
+    }
+
+}
